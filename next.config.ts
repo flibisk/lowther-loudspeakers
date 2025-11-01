982edf3d0f2a806fee0b7cfdd32bf16f29bf4a83
Peter Snell <flibisk@Peters-MacBook-Pro.local>
2025-11-01 13:04:41 -0500

    Use unoptimized images for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.beehiiv.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'beehiiv-images-production.s3.amazonaws.com',
-      },
-    ],
-    domains: ['media.beehiiv.com', 'beehiiv-images-production.s3.amazonaws.com'],
+    unoptimized: true,
   },
 };
 
