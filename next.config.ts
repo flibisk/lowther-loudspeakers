e91d9945561354ff23be4fc2d47082995b85914a
Peter Snell <flibisk@Peters-MacBook-Pro.local>
2025-11-01 13:02:13 -0500

    Simplify Next.js image config for Vercel compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,25 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'media.beehiiv.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.beehiiv.com',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'beehiiv-images-production.s3.amazonaws.com',
-        pathname: '/**',
       },
     ],
+    domains: ['media.beehiiv.com', 'beehiiv-images-production.s3.amazonaws.com'],
   },
 };
 
