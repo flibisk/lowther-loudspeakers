8f09cfefec85e78331edf8c0280e26cd819e2b2f
Peter Snell <flibisk@Peters-MacBook-Pro.local>
2026-01-08 12:00:56 -0600

    fix: update Prisma type declaration to resolve module correctly
    - Declare .prisma/client/default module to match @prisma/client exports
    - This should allow TypeScript to resolve PrismaClient correctly
    - Fixes build error where PrismaClient couldn't be imported

--- src/types/prisma.d.ts
+++ src/types/prisma.d.ts
@@ -1,5 +1,9 @@
-/// <reference types=".prisma/client" />
+declare module '.prisma/client/default' {
+  export * from '../../node_modules/.prisma/client/client';
+  export * from '../../node_modules/.prisma/client/enums';
+  export * from '../../node_modules/.prisma/client/models';
+}
 
 declare module '@prisma/client' {
-  export * from '.prisma/client';
+  export * from '.prisma/client/default';
 }
